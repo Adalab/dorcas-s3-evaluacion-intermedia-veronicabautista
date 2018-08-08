431c9b4ea9b71c58a7030158643c5b04955076c4
Veronica Bautista <veronicabautista87@gmail.com>
2018-08-08 17:26:42 +0200

    pokedex con buscador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import './App.css';
 import PokemonList from './components/PokemonList';
+import Search from './components/Search';
+
+// <--Futura prueba con API-->
+// const apiList = "https://pokeapi.co/api/v2/pokemon/?limit=151";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      poke: [],
+      name: "",
+    }
+
+   this.searchPokemon = this.searchPokemon.bind(this);
+  }
+
+  // componentDidMount () {
+  //   this.fetchCharacterList()
+  // }
+
+
+  // fetchCharacterList (){
+  //   fetch (apiList)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     this.setState({
+  //       poke: data
+  //     });
+  //   });
+
+  // }
+
+  searchPokemon(event) {
+    const myPoke = event.currentTarget.value;
+
+    this.setState({
+      name: myPoke
+    });
+  }
+
   render() {
     return (
       <div className="kanto_card">
         <h1 className="title">Pokédex Kanto</h1>
-        <PokemonList pokemonKanto={this.props.pokedex}/>
+        <Search
+            pokeSearch={this.searchPokemon} />
+        <PokemonList pokemonKanto={this.props.pokedex} pokeName={this.state.name}/>
       </div>
     );
   }
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -5,7 +5,11 @@ class PokemonList extends React.Component {
   render () {
     return (
 			<ul className="pokemonlist">
-          {this.props.pokemonKanto.map(card => {
+          {this.props.pokemonKanto
+          .filter(searching => {
+            return searching.name.toLocaleLowerCase().includes(this.props.pokeName.toLocaleLowerCase());
+            })
+          .map(card => {
             return (
               <li className="pokemonlist__card" key={card.id}>
                 <PokeCard pokemon={card} />
@@ -18,3 +22,29 @@ class PokemonList extends React.Component {
 };
 
 export default PokemonList;
+
+// class CharacterList extends React.Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<ul>
+// 					{this.props.character
+// 						.filter(person => {
+// 							return person.name.toLocaleLowerCase().includes(this.props.personName.toLocaleLowerCase());
+// 						})
+// 						.map(item => {
+// 							return (
+// 								<li className="character__card">
+// 									<img className="character__picture" src={item.image} alt={item.name} />
+// 									<h2 className="character_name">{item.name}</h2>
+// 									<h3 className="character__house">{item.house}</h3>
+// 								</li>
+// 							);
+// 						})}
+//         </ul>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default CharacterList;
